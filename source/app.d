15e0e203aa354788540c1ff493b0fa5ce1829fba
Kazuya Takahashi <electrolysis.jp@gmail.com>
2019-01-03 02:07:23 +0900

    Enable click'n'focus on entire frame
    Good reference:
    linux - xcb: Asynchronous pointer grab not propagating events - Unix & Linux Stack Exchange
    https://unix.stackexchange.com/questions/397339/xcb-asynchronous-pointer-grab-not-propagating-events

--- source/app.d
+++ source/app.d
@@ -57,13 +57,19 @@ class Redbat
 
         manageChildrenOfRoot();
 
+        xcb_grab_button(connection, 0, rootWindow, XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE,
+                XCB_GRAB_MODE_SYNC, XCB_GRAB_MODE_ASYNC, XCB_NONE, XCB_NONE, XCB_BUTTON_INDEX_ANY, XCB_MOD_MASK_ANY);
+
         while (true)
         {
-            auto event = xcb_wait_for_event(connection);
+            xcb_allow_events(connection, XCB_ALLOW_REPLAY_POINTER, XCB_CURRENT_TIME);
+            auto event = xcb_poll_for_event(connection);
             if (event is null)
             {
-                error("I/O error");
-                break;
+                import core.thread;
+
+                Thread.sleep(20.dur!"msecs"); // Save the earth!!
+                continue;
             }
 
             immutable eventType = event.response_type & ~0x80;
@@ -187,16 +193,25 @@ class Redbat
 
     void onButtonPress(xcb_button_press_event_t* event)
     {
-        // XXX: assume event.event to be frame
-        info(*event);
-        if (auto mem_p = event.event in frameMembersOf)
+        // XXX: assume event.event to be root
+        infof("%#x %#x", event.event, event.child);
+        if (event.event != rootWindow)
+        {
+            return;
+        }
+
+        if (auto mem_p = event.child in frameMembersOf)
         {
             infof("Set focus: %#x", mem_p.window);
             xcb_set_input_focus(connection, XCB_INPUT_FOCUS_POINTER_ROOT, mem_p.window, event.time);
             immutable uint v = XCB_STACK_MODE_ABOVE;
-            xcb_configure_window(connection, event.event, XCB_CONFIG_WINDOW_STACK_MODE, &v);
-            xcb_flush(connection);
+            xcb_configure_window(connection, event.child, XCB_CONFIG_WINDOW_STACK_MODE, &v);
         }
+        else
+        {
+            infof("Button presse event is detected above unmanaged window %#x", event.child);
+        }
+        xcb_flush(connection);
     }
 
     void onButtonRelease(xcb_button_release_event_t* event)
@@ -247,7 +262,7 @@ class Redbat
     xcb_window_t createFrame(short x, short y, ushort width, ushort height)
     {
         auto frame = xcb_generate_id(connection);
-        immutable uint mask = XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY | XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT;
+        immutable uint mask = XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY | XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT;
         xcb_create_window(connection, XCB_COPY_FROM_PARENT, frame, rootWindow, x, y, width, height, frameBorderWidth,
                 XCB_WINDOW_CLASS_INPUT_OUTPUT, screen.root_visual, XCB_CW_EVENT_MASK, &mask);
         xcb_flush(connection);
